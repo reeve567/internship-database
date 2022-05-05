55b179b56c8d4135535d18983742412e80571bd1
Reeve <reeveblake@gmail.com>
2022-05-05 19:24:09 -0400

    main CRUD start

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 const path = require("path");
 const express = require("express");
+const sqlite3 = require("sqlite3");
+
+const db = new sqlite3.Database("./sql/db.sqlite");
+
 const app = express()
 
 
+app.use(express.static(path.join(__dirname, "web")))
 app.use("/", express.static("index.html"))
 
-app.get("/api", function (req, res) {
-
-})
+app.get("/api/searchPositionsByCompany/:company", function (req, res) {
+	db.all("select * from ", function () {
+		
+	})
+});
 
 app.listen(8080);
 
